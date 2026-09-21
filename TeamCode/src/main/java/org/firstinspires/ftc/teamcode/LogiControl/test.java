cd80a354c08c67a6f3db06f0d6c12d8a6cedcb4e
Ethan Repasky <erepasky2028@csyonline.com>
2026-09-21 15:41:13 -0400

    Add cycle time code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LogiControl/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LogiControl/test.java
@@ -5,9 +5,9 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class test {
-    DcMotor FL = hardwareMap.get(DcMotor.class, "FL");
-    DcMotor FR = hardwareMap.get(DcMotor.class, "FR");
-    DcMotor BL = hardwareMap.get(DcMotor.class, "BL");
-    DcMotor BR = hardwareMap.get(DcMotor.class, "BR");
-    PID pid = new PID(1,0,0,)
+//    DcMotor FL = hardwareMap.get(DcMotor.class, "FL");
+//    DcMotor FR = hardwareMap.get(DcMotor.class, "FR");
+//    DcMotor BL = hardwareMap.get(DcMotor.class, "BL");
+//    DcMotor BR = hardwareMap.get(DcMotor.class, "BR");
+//    PID pid = new PID(1,0,0,)
 }
